53179d0ca7c4d8223542c7d8858eebc0d718fdda
Dylan Thomas <thomasdylan76@gmail.com>
2020-06-29 19:55:00 -0600

    Form created

--- client/src/App.js
+++ client/src/App.js
@@ -4,20 +4,25 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import logo from "./logo.svg";
 import Home from './Views/Home';
 import Login from './Views/Login';
+import Register from './Views/Register';
 import "./App.scss";
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Router>
-          <header>
+          <div>
             <Navbar />
-          </header>
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/login" component={Login} />
-          </Switch>
+          </div>
+          <div>
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <Route path="/login" component={Login} />
+              <Route path="/register" component={Register} />
+            </Switch>
+          </div>
         </Router>
       </div>
     );
--- models/user.js
+++ models/user.js
@@ -1,9 +1,31 @@
+const { Sequelize } = require("sequelize");
+
 module.exports = function (sequelize, DataTypes) {
     var User = sequelize.define("User", {
-        email: DataTypes.STRING,
-        password: DataTypes.STRING,
-        firstName: DataTypes.STRING,
-        lastName: DataTypes.STRING
+        id: {
+            autoIncrement: true,
+            primaryKey: true,
+            type: DataTypes.INTEGER
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        firstName: {
+            type: DataTypes.STRING,
+            notEmpty: true
+        },
+        lastName: {
+            type: DataTypes.STRING,
+            notEmpty: true
+        }
     });
     return User;
 };
